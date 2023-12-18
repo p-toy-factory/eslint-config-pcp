b97bc11fa00ccd7e187187ee7019ed304e508652
Pink Champagne <45930107+PinkChampagne17@users.noreply.github.com>
2023-12-18 15:33:40 +0800

    chore: initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,1 @@
+module.exports = require("@pinkchampagne/prettier-config");
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from "./configs";
+export { buildConfig } from "./factory";
+export { BuildConfigOptions } from "./types";
